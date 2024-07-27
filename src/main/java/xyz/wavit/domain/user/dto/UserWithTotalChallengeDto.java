6e2fa3bbb039217cb439d31b848f1eba084ef164
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-28 03:49:23 +0900

    refactor: Long으로 변경

--- src/main/java/xyz/wavit/domain/user/dto/UserWithTotalChallengeDto.java
+++ src/main/java/xyz/wavit/domain/user/dto/UserWithTotalChallengeDto.java
@@ -2,9 +2,9 @@ package xyz.wavit.domain.user.dto;
 
 import xyz.wavit.domain.user.domain.User;
 
-public record UserWithTotalChallengeDto(UserFullDto user, int totalChallengeCount) {
+public record UserWithTotalChallengeDto(UserFullDto user, Long totalChallengeCount) {
 
-    public static UserWithTotalChallengeDto from(User user, int totalChallengeCount) {
+    public static UserWithTotalChallengeDto from(User user, Long totalChallengeCount) {
         return new UserWithTotalChallengeDto(UserFullDto.from(user), totalChallengeCount);
     }
 }
