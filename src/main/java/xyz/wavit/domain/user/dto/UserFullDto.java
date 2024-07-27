9cd4c645991c6eb89e87a10bef08c3d3d402eaeb
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-28 03:14:21 +0900

    feat: UserFullDto에 프로필 URL 추가

--- src/main/java/xyz/wavit/domain/user/dto/UserFullDto.java
+++ src/main/java/xyz/wavit/domain/user/dto/UserFullDto.java
@@ -3,12 +3,19 @@ package xyz.wavit.domain.user.dto;
 import xyz.wavit.domain.user.domain.User;
 import xyz.wavit.domain.user.domain.UserRole;
 
-public record UserFullDto(Long userId, UserRole role, String name, String nickname, String username) {
+public record UserFullDto(
+        Long userId, UserRole role, String name, String nickname, String username, String profileImageUrl) {
     public static UserFullDto from(User user) {
-        return new UserFullDto(user.getId(), user.getRole(), user.getName(), user.getNickname(), user.getUsername());
+        return new UserFullDto(
+                user.getId(),
+                user.getRole(),
+                user.getName(),
+                user.getNickname(),
+                user.getUsername(),
+                user.getProfileImageUrl());
     }
 
     public static UserFullDto createEmpty() {
-        return new UserFullDto(null, null, null, null, null);
+        return new UserFullDto(null, null, null, null, null, null);
     }
 }
