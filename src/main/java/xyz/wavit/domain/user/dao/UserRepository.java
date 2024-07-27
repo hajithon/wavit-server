b8ad1032d3d2f9030a494513b5e0f4886972f631
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-27 20:23:59 +0900

    feat: 유저 레포지터리 추가

---
+++ src/main/java/xyz/wavit/domain/user/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package xyz.wavit.domain.user.dao;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import xyz.wavit.domain.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
