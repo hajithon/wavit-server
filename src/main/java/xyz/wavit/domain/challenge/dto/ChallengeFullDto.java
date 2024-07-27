a7831f99bcf401332ed673c8b8b78f6135f5db66
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-28 02:50:24 +0900

    feat: 챌린지 DTO에 인증 정보 및 NPE 방어 로직 추가

--- src/main/java/xyz/wavit/domain/challenge/dto/ChallengeFullDto.java
+++ src/main/java/xyz/wavit/domain/challenge/dto/ChallengeFullDto.java
@@ -2,6 +2,7 @@ package xyz.wavit.domain.challenge.dto;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import xyz.wavit.domain.challenge.domain.Challenge;
 import xyz.wavit.domain.user.dto.UserFullDto;
 
@@ -10,17 +11,24 @@ public record ChallengeFullDto(
         LocalDateTime startAt,
         LocalDateTime finishAt,
         Duration remainingTime,
-        UserFullDto challengedBy) {
+        UserFullDto challengedBy,
+        String imageUrl,
+        String comment) {
     public static ChallengeFullDto from(Challenge challenge) {
+        // 시스템에 의해 생성된 챌린지의 경우 challengedBy가 null이므로 Optional로 감싸서 처리
         return new ChallengeFullDto(
                 challenge.getId(),
                 challenge.getStartAt(),
                 challenge.getFinishAt(),
                 challenge.getRemainingTime(),
-                UserFullDto.from(challenge.getChallengedBy()));
+                Optional.ofNullable(challenge.getChallengedBy())
+                        .map(UserFullDto::from)
+                        .orElse(UserFullDto.createEmpty()),
+                challenge.getImageUrl(),
+                challenge.getComment());
     }
 
     public static ChallengeFullDto createEmpty() {
-        return new ChallengeFullDto(null, null, null, null, null);
+        return new ChallengeFullDto(null, null, null, null, UserFullDto.createEmpty(), null, null);
     }
 }
