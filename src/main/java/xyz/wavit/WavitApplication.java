c664c83138a9a6eceda078606321a6c0d7b418ec
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-27 18:16:48 +0900

    init: 프로젝트 생성

---
+++ src/main/java/xyz/wavit/WavitApplication.java
@@ -0,0 +1,12 @@
+package xyz.wavit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WavitApplication.class, args);
+    }
+}
---
+++ src/test/java/xyz/wavit/WavitApplicationTests.java
@@ -0,0 +1,11 @@
+package xyz.wavit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavitApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
