3de7c7f04c0387b395d0ccda2435dcc80cfe4c5b
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-27 19:29:33 +0900

    feat: JwtService 구현

--- src/main/java/xyz/wavit/domain/auth/application/JwtService.java
+++ src/main/java/xyz/wavit/domain/auth/application/JwtService.java
@@ -4,13 +4,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import xyz.wavit.domain.auth.dto.AccessTokenDto;
+import xyz.wavit.domain.user.domain.UserRole;
+import xyz.wavit.global.util.JwtUtil;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtService {
 
+    private final JwtUtil jwtUtil;
+
+    public AccessTokenDto createAccessToken(Long userId, UserRole userRole) {
+        return jwtUtil.generateAccessToken(userId, userRole);
+    }
+
     public AccessTokenDto parseAccessToken(String accessToken) {
-        return null;
+        return jwtUtil.parseAccessToken(accessToken);
     }
 }
