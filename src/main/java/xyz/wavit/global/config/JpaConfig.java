64883895bd544167b391803fa436911ea5a07a44
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-27 20:03:42 +0900

    chore: Jpa Auditing 설정

---
+++ src/main/java/xyz/wavit/global/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package xyz.wavit.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
