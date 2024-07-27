681aacca9ea1798dce188a846f4076b55ad7080f
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-28 01:34:10 +0900

    feat: 챌린지 생성 정책 적용

--- src/main/java/xyz/wavit/domain/challenge/application/ChallengeService.java
+++ src/main/java/xyz/wavit/domain/challenge/application/ChallengeService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import xyz.wavit.domain.challenge.dao.ChallengeRepository;
 import xyz.wavit.domain.challenge.domain.Challenge;
+import xyz.wavit.domain.challenge.domain.ChallengeValidator;
 import xyz.wavit.domain.challenge.dto.ChallengeCreateRequest;
 import xyz.wavit.domain.user.dao.UserRepository;
 import xyz.wavit.domain.user.domain.User;
@@ -20,10 +21,13 @@ public class ChallengeService {
     private final UserUtil userUtil;
     private final UserRepository userRepository;
     private final ChallengeRepository challengeRepository;
+    private final ChallengeValidator challengeValidator;
 
     @Transactional
     public void createChallengeBySystem(ChallengeCreateRequest request) {
         List<User> nominatedUsers = userRepository.findAllById(request.nominatedUserIds());
+
+        validateAlreadyNominated(nominatedUsers);
 
         List<Challenge> challenges =
                 nominatedUsers.stream().map(Challenge::createBySystem).toList();
@@ -39,6 +43,8 @@ public class ChallengeService {
 
         List<User> nominatedUsers = userRepository.findAllById(request.nominatedUserIds());
 
+        validateAlreadyNominated(nominatedUsers);
+
         List<Challenge> challenges = nominatedUsers.stream()
                 .map(nominatee -> Challenge.createByUser(nominator, nominatee))
                 .toList();
@@ -46,5 +52,12 @@ public class ChallengeService {
         challengeRepository.saveAll(challenges);
 
         log.info("[ChallengeService] 사용자에 의한 챌린지 생성: ids={}", request.nominatedUserIds());
+    }
+
+    private void validateAlreadyNominated(List<User> nominatedUsers) {
+        nominatedUsers.forEach(nominee -> {
+            var challengesByNominee = challengeRepository.findByChallengedUser(nominee);
+            challengeValidator.validateAlreadyNominated(challengesByNominee);
+        });
     }
 }
