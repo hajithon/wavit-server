b3e9b5d2e86c80fbf0639cf69dde0f3a89f05a86
Jaehyun Ahn <desktop3711@gmail.com>
2024-07-28 02:26:24 +0900

    feat: 현재 미완료 챌린지 조회 API 구현

--- src/main/java/xyz/wavit/domain/challenge/api/ChallengeController.java
+++ src/main/java/xyz/wavit/domain/challenge/api/ChallengeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.wavit.domain.challenge.application.ChallengeService;
 import xyz.wavit.domain.challenge.dto.ChallengeCreateRequest;
+import xyz.wavit.domain.challenge.dto.ChallengeIncompleteDto;
 
 @Tag(name = "[Challenge]", description = "챌린지 API")
 @RestController
@@ -32,5 +33,12 @@ public class ChallengeController {
     public ResponseEntity<Void> createChallengeByUser(@Valid @RequestBody ChallengeCreateRequest request) {
         challengeService.createChallengeByUser(request);
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "현재 미완료 챌린지 조회", description = "현재 사용자가 수행하고 있는 미완료 챌린지를 조회합니다. 없는 경우 빈 응답을 반환합니다.")
+    @PostMapping("/my/incomplete")
+    public ResponseEntity<ChallengeIncompleteDto> getMyIncompleteChallenge() {
+        var response = challengeService.getMyIncompleteChallenge();
+        return ResponseEntity.ok(response);
     }
 }
